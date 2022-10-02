00ce265aa1fe84f978c1b1a901ecb286ca4436e3
Poolce <poolce@list.ru>
2022-10-03 02:18:56 +0300

    Готовая версия, исправленны ошибки.

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
-from heapq import merge
 import source.merge_sort_four as mg
 import source.avl_tree.avl as avl
 import random
+import matplotlib.pyplot as plt
+from timeit import default_timer as timer
+
+
+avl_sort_n = []
+avl_sort_time = []
+
+merge_data_time = []
+merge_data_n = []
 
 #Создание списка
-for i in range(100,10000,100):
-    
+for i in range(0,1000,2):
+
     avl_a = avl.avl_tree()
     rt = None
-
     sorting_list_by_merge = []
+    
+
     for j in range(i):
-        rand = random.randint(100,1000)
+        rand = random.randint(-10000000,10000000)
         rt = avl_a.insert(rand,rt)
         sorting_list_by_merge.append(rand)
 
 
-    print(i/100)
+    print(i)
     #Сама сортировка на АВЛ
-    #avl_sorted_list = avl_a.sort_list_by_avl(rt)
+    start_timer = timer()
+    avl_sorted_list = avl_a.sort_list_by_avl(rt)
+    avl_sort_time.append(timer()-start_timer)
+    avl_sort_n.append(i)
+
 
     #Сортировка слиянием
+    start_timer = timer()
     merge_sorted_list = mg.merge_sort(sorting_list_by_merge)
+    merge_data_time.append(timer()-start_timer)
+    merge_data_n.append(i)
+
+print(avl_sort_time,'t',avl_sort_n,'n')
+plt.plot(merge_data_n,merge_data_time,'ro')
+plt.plot(avl_sort_n,avl_sort_time,'b*')
+plt.ylabel('some numbers')
+plt.show()
